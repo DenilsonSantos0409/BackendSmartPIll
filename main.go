9f2c48165b330e6c7a4e8fbb89d9d4afff76530f
DenilsonSantos0409 <denilsonsantos0409@gmail.com>
2025-06-20 06:10:32 +0800

    perbaikan main.go

--- main.go
+++ main.go
@@ -4,11 +4,20 @@ import (
 	"log"
 	"net/http"
 
-	db "smartpill/connection"
+	db "smartpill/connection" 
+	"smartpill/schema"
+	"github.com/graphql-go/handler"
 )
 
 func main() {
 	db.ConnectDatabase()
+	h := handler.New(&handler.Config{
+		Schema:   &schema.Schema,
+		Pretty:   true,
+		GraphiQL: true,
+	})
+	http.Handle("/graphql", h)
 
+	log.Println("🚀 GraphQL siap di http://localhost:8080/graphql")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
